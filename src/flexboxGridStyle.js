516012fb3268f3a9c16a6cdb5e37002f1bcc682f
Santhosh Kumar <santhoshmp89@gmail.com>
2020-04-21 23:31:38 +0530

    Grid structure added

--- src/flexboxGridStyle.js
+++ src/flexboxGridStyle.js
@@ -2,8 +2,15 @@ import styled from "styled-components";
 
 export const FlexContainer = styled.div`
   display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
 export const FlexGridItem = styled.div`
-  display: block;
+  padding: 10px;
+  margin: 10px;
+  background: #000;
+  color: #fff;
+  flex: 0 1 20%;
+  box-sizing: border-box;
 `;
